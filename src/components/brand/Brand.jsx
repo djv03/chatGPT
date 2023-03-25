71932df93ffa4acd8465501b24b60f97c0d545d7
dhruvin <dhruvin.200410116086@gmail.com>
2023-03-25 16:41:37 +0530

    mobile view improved

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-// import { google, slack, atlassian, dropbox, shopify } from './imports';
+import google from '../../../Assets/google.png';
+import atlassian from '../../../Assets/atlassian.png';
+import dropbox from '../../../Assets/dropbox.png';
+import shopify from '../../../Assets/shopify.png';
+import slack from '../../../Assets/slack.png';
 
 import './brand.css';
 
 const Brand = () => (
   <div className="gpt3__brand section__padding">
     <div>
-      <img src="" alt="alt image" />
+      <img src={google} alt="alt image" />
     </div>
     <div>
-      <img src="" alt="alt image" />
+      <img src={atlassian} alt="alt image" />
     </div>
     <div>
-      <img src="" alt="alt image" />
+      <img src={dropbox} alt="alt image" />
     </div>
     <div>
-      <img src="" alt="alt image" />
+      <img src={shopify} alt="alt image" />
     </div>
     <div>
-      <img src="" alt="alt image" />
+      <img src={slack} alt="alt image" />
     </div>
   </div>
 );
